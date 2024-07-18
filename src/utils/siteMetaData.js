a1a598606f04c2eab5a249258f1894868619d899
Rukesh2274 <20113119@student.hindustanuniv.ac.in>
2024-07-18 14:29:46 +0530

    production release

---
+++ src/utils/siteMetaData.js
@@ -0,0 +1,21 @@
+// please update required information
+const siteMetadata = {
+    title: 'Next.js Blog With Tailwind CSS and Contentlayer',
+    author: 'Trivino',
+    headerTitle: 'Next.js Blog',
+    description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
+    language: 'en-us',
+    theme: 'system', // system, dark or light
+    siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
+    siteLogo: '/logo.png',
+    socialBanner: '/social-banner.png', // add social banner in the public folder
+    email: 'trukeshreddy79@gmail.com', 
+    twitter: 'https://twitter.com/',
+    facebook: 'https://facebook.com',
+    youtube: 'https://youtube.com/codebucks',
+    linkedin: 'https://www.linkedin.com/in/rukesh2274/',
+    dribbble: 'https://www.dribbble.com',
+    locale: 'en-US',
+  }
+  
+  module.exports = siteMetadata
